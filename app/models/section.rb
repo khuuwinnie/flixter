7d855e305bcff5a3b278b81ae209d960d43ee555
Winnie Khuu <khuu.winnie@gmail.com>
2019-08-27 03:18:33 +0000

    Fixed next lesson after end of section

--- app/models/section.rb
+++ app/models/section.rb
@@ -7,9 +7,6 @@ class Section < ApplicationRecord
 
   def next_section
     section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
-    if lesson.blank && section.next_section
-      return section.next_section.lessons.rank(:row_order).first
-    end
     return section
   end
 end
